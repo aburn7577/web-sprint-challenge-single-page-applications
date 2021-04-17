5cab74ca0d8a00c7d9dfc09af8a184cdf4bcc049
Ashley Burns <ash_burn999@yahoo.com>
2021-04-17 15:29:04 -0400

    form done need to add errors, validation, and return from database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from 'react-router-dom'
 import Homepage from './components/homepage'
 import Order from './components/order'
+import Axios from 'axios'
+
+const startFormValues = {
+  // text inputs//
+  name: '',
+  special: '',
+  // Dropdown
+  size: '',
+  // Checkbox min 4
+  pepperoni: false,
+  sausage: false,
+  bacon: false,
+  onion: false,
+  extraCheese: false,
+  pinapple: false,
+}
+
+const startOrders = []
+const startDisabled = true
+
 
 const App = () => {
+  const [orders, setOrders] = useState(startOrders)
+  const [formValues, setFormValues] = useState(startFormValues)
+  const [disabled, setDisabled] = useState(startDisabled)
+
+  const getOrders = () => {
+    Axios.get('https://reqres.in/api/order')
+      .then(res => {
+        setOrders(res.data)
+      })
+      .catch(error => {
+        console.log(error)
+        console.log('get orders didnt work')
+      })
+  }
+  const postNewOrder = newOrder => {
+    Axios.post('https://reqres.in/api/order', newOrder)
+      .then(res => {
+        setOrders([...orders, res.data])
+        setFormValues(startFormValues)
+      })
+      .catch(error => {
+        setFormValues(startFormValues)
+        console.log(error)
+        console.log('postNewOrder didnt work')
+      })
+  }
+  //Event Handlers
+  const inputChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value })
+  }
+  const formSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      special: formValues.special.trim(),
+      size: formValues.size,
+      toppings: ['pepperoni', 'sausage', 'bacon',
+        'onion', 'extraCheese', 'pinapple'].filter(top => formValues[top])
+    }
+    postNewOrder(newOrder)
+  }
+  //side Effects
+  useEffect(() => {
+    getOrders()
+  }, [])
+
+  useEffect(() => {
+
+  }, [])
+
 
 
   return (
@@ -19,7 +88,13 @@ const App = () => {
           <Homepage />
         </Route>
         <Route path='/pizza'>
-          <Order />
+          <Order
+            values={formValues}
+            change={inputChange}
+            submit={formSubmit}
+          // disabled={disabled}
+          // errors={formErrors}
+          />
         </Route>
       </Switch>
 
--- src/components/order.js
+++ src/components/order.js
@@ -1,14 +1,104 @@
 import React from 'react'
 
 
-export default function Order() {
+export default function Order(props) {
+    const { values, change, submit, disabled, errors } = props
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+    const onChange = evt => {
+        const { name, value, type, checked } = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        change(name, valueToUse)
+    }
 
     return (
-        <form className='App'>
+        <form className='App' onSubmit={onSubmit}>
             <div>
                 <h2>Build Your Own Pizza</h2>
             </div>
+            <div>
+                <h4>Your info</h4>
+                <label>Name:
+                    <input
+                        value={values.name}
+                        onChange={onChange}
+                        name='name'
+                        type='text' />
+                </label>
+            </div>
+            <div>
+                <h4>Your Pizza Build</h4>
+                <label>Choice of size
+                    <select
+                        onChange={onChange}
+                        value={values.size}
+                        name='size'>
+                        <option value=''>Select a size</option>
+                        <option value='12'> 12 inch</option>
+                        <option value='14'> 14 inch</option>
+                        <option value='18'> 18 inch</option>
+                        <option value='half'>Half Sheet</option>
+                        <option value='full'>Full Sheet</option>
+                    </select>
+                </label>
+                <h4>Pick your toppings</h4>
+                <label>Pepperoni
+                        <input
+                        type='checkbox'
+                        name='pepperoni'
+                        checked={values.pepperoni}
+                        onChange={onChange} />
+                </label>
+                <label>Sausage
+                        <input
+                        type='checkbox'
+                        name='sausage'
+                        checked={values.sausage}
+                        onChange={onChange} />
+                </label>
+                <label>Bacon
+                        <input
+                        type='checkbox'
+                        name='bacon'
+                        checked={values.bacon}
+                        onChange={onChange} />
+                </label>
+                <label>Onion
+                        <input
+                        type='checkbox'
+                        name='onion'
+                        checked={values.onion}
+                        onChange={onChange} />
+                </label>
+                <label>Extra cheese
+                        <input
+                        type='checkbox'
+                        name='extraCheese'
+                        checked={values.extraCheese}
+                        onChange={onChange} />
+                </label>
+                <label>Pinapple
+                        <input
+                        type='checkbox'
+                        name='pinapple'
+                        checked={values.pinapple}
+                        onChange={onChange} />
+                </label>
+            </div>
+            <div>
+                <label>Special Instructions
+                        <input
+                        value={values.special}
+                        onChange={onChange}
+                        name='special'
+                        type='text' />
+                </label>
+            </div>
 
+            <button>Add to Order</button>
         </form>
     )
 }
